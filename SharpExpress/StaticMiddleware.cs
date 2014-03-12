a0dd7bb1f9c4aac452c745c6419200809d37928a
sergeyt <stodyshev@gmail.com>
2014-03-12 10:00:35 +0700

    static middleware api (issue #2)

---
+++ SharpExpress/StaticMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SharpExpress
+{
+	/// <summary>
+	/// Static middleware to serve static files.
+	/// </summary>
+	public static class StaticMiddleware
+	{
+		/// <summary>
+		/// Serves specified directory
+		/// </summary>
+		/// <param name="app">The application to extend.</param>
+		/// <param name="url"></param>
+		/// <param name="dir"></param>
+		/// <returns></returns>
+		public static ExpressApplication Static(this ExpressApplication app, string url, string dir)
+		{
+			return app.Get(url, req =>
+			{
+				throw new NotImplementedException();
+			});
+		}
+	}
+}
