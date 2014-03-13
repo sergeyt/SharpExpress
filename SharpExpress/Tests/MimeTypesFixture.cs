58c8e452411e083717d021f8427c090486d5825b
sergeyt <stodyshev@gmail.com>
2014-03-14 00:10:59 +0700

    mime types (issue #9)

---
+++ SharpExpress/Tests/MimeTypesFixture.cs
@@ -0,0 +1,27 @@
+﻿#if NUNIT
+using NUnit.Framework;
+
+namespace SharpExpress.Tests
+{
+	[TestFixture]
+	public class MimeTypesFixture
+	{
+		[TestCase(".css", Result = "text/css")]
+		[TestCase(".html", Result = "text/html")]
+		[TestCase(".htm", Result = "text/html")]
+		[TestCase(".js", Result = "application/javascript")]
+		[TestCase(".json", Result = "application/json")]
+		[TestCase(".xml", Result = "application/xml")]
+		[TestCase("css", Result = "text/css")]
+		[TestCase("html", Result = "text/html")]
+		[TestCase("htm", Result = "text/html")]
+		[TestCase("js", Result = "application/javascript")]
+		[TestCase("json", Result = "application/json")]
+		[TestCase("xml", Result = "application/xml")]
+		public string ByExtension(string ext)
+		{
+			return MimeType.ByExtension(ext);
+		}
+	}
+}
+#endif
