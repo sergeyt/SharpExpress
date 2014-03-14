dc1e3c03159e67ceb4c1f07718c1976f6d038ef9
sergeyt <stodyshev@gmail.com>
2014-03-14 23:56:56 +0700

    multi http handler (issue #13)

---
+++ SharpExpress/MultiHttpHandler.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace SharpExpress
+{
+	/// <summary>
+	/// Composite HTTP handler.
+	/// </summary>
+	public sealed class MultiHttpHandler : IHttpHandler, IEnumerable<KeyValuePair<string, IHttpHandler>>
+	{
+		private struct Entry
+		{
+			public readonly string Path;
+			public readonly IHttpHandler Handler;
+			private readonly Regex _regex;
+
+			public Entry(string path, IHttpHandler handler)
+			{
+				Path = path;
+				Handler = handler;
+				_regex = WildcardToRegex(path);
+			}
+
+			public IHttpHandler Match(HttpContext context)
+			{
+				// TODO use VB LikeString function if it is faster
+				return _regex.IsMatch(context.Request.Path) ? Handler : null;
+			}
+
+			private static Regex WildcardToRegex(string pattern)
+			{
+				return new Regex(
+					"^" + Regex.Escape(pattern).Replace(@"\*", ".*").Replace(@"\?", ".") + "$"
+					, RegexOptions.Compiled | RegexOptions.IgnoreCase
+					);
+			}
+		}
+
+		private readonly List<Entry> _entries = new List<Entry>();
+
+		public void Add(string path, IHttpHandler handler)
+		{
+			_entries.Add(new Entry(path, handler));
+		}
+
+		public void ProcessRequest(HttpContext context)
+		{
+			var handler = _entries.Select(x => x.Match(context)).FirstOrDefault() ?? NotFound;
+			handler.ProcessRequest(context);
+		}
+
+		public bool IsReusable
+		{
+			get { return true; }
+		}
+
+		public IEnumerator<KeyValuePair<string, IHttpHandler>> GetEnumerator()
+		{
+			foreach (var entry in _entries)
+			{
+				yield return new KeyValuePair<string, IHttpHandler>(entry.Path, entry.Handler);
+			}
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+
+		private static readonly IHttpHandler NotFound = new NotFoundHandler();
+
+		private class NotFoundHandler : IHttpHandler
+		{
+			public void ProcessRequest(HttpContext context)
+			{
+				var res = context.Response;
+				res.StatusCode = (int) HttpStatusCode.NotFound;
+				res.ContentType = "text/plain";
+				res.Write(string.Format("Resource '{0}' not found!", context.Request.Path));
+				res.End();
+			}
+
+			public bool IsReusable { get; private set; }
+		}
+	}
+}
