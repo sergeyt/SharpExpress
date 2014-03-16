7203a11ae72adf73d75eac90b88ef845b31027a5
sergeyt <stodyshev@gmail.com>
2014-03-17 00:39:24 +0700

    spark (issue #18)

---
+++ src/SharpExpress.Spark/SparkEngine.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using Spark;
+
+namespace SharpExpress.Spark
+{
+	public class SparkEngine : IViewEngine
+	{
+		private readonly SparkViewEngine _engine;
+
+		public SparkEngine()
+		{
+			// TODO read settings from config
+			var settings = new SparkSettings
+			{
+				DefaultLanguage = LanguageType.CSharp
+			};
+			settings.SetPageBaseType(typeof(ViewBase));
+			_engine = new SparkViewEngine();
+		}
+
+		public void Render(Template template, object data, TextWriter output)
+		{
+			var descriptor = new SparkViewDescriptor().AddTemplate(template.Name + ".spark");
+
+			var view = (ViewBase)_engine.CreateInstance(descriptor);
+			try
+			{
+				view.Data = data;
+				view.RenderView(output);
+			}
+			finally
+			{
+				_engine.ReleaseInstance(view);
+			}
+		}
+	}
+
+	internal abstract class ViewBase : AbstractSparkView
+	{
+		public object Data { get; set; }
+
+		// TODO Eval for late binding
+		public object Eval(string expression)
+		{
+			// TODO
+			return null;
+		}
+
+		public string Eval(string expression, string format)
+		{
+			// TODO
+			return null;
+		}
+	}
+}
---
+++ src/SharpExpress/IViewEngine.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+namespace SharpExpress
+{
+	public interface IViewEngine
+	{
+		void Render(Template template, object data, TextWriter output);
+	}
+
+	public struct Template
+	{
+		public readonly string Name;
+		public readonly string Content;
+
+		public Template(string name, string content)
+		{
+			Name = name;
+			Content = content;
+		}
+	}
+}
