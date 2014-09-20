fc94803250bd6c497064212342707d6f0d62f81f
sergeyt <stodyshev@gmail.com>
2014-09-20 10:21:37 +0700

    simplified HttpServer implementation

--- src/SharpExpress/HttpServer.cs
+++ src/SharpExpress/HttpServer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Web;
 
 namespace SharpExpress
@@ -8,7 +9,11 @@ namespace SharpExpress
 	/// </summary>
 	public sealed class HttpServer : MarshalByRefObject, IDisposable
 	{
-		private readonly ServerImpl _server;
+		private readonly IHttpListener _listener;
+		private readonly Thread _listenerThread;
+		private readonly ManualResetEvent _stop = new ManualResetEvent(false);
+		private readonly object _lock = new object();
+		private bool _stoped;
 
 		public HttpServer(IHttpHandler app, HttpServerSettings settings)
 		{
@@ -35,13 +40,63 @@ namespace SharpExpress
 				default:
 					throw new ArgumentOutOfRangeException();
 			}
-			
-			_server = new ServerImpl(listener, settings.WorkerCount);
+
+			_listener = listener;
+			_listener.Start();
+
+			_listenerThread = new Thread(Listen);
+			_listenerThread.Start();
 		}
 
 		public void Dispose()
 		{
-			_server.Stop();
+			Stop();
+		}
+
+		private void Stop()
+		{
+			_stop.Set();
+			_listenerThread.Join();
+
+			lock (_lock)
+			{
+				_listener.Stop();
+				_listener.Close();
+				_stoped = true;
+			}
+		}
+
+		private void Listen()
+		{
+			while (_listener.IsListening)
+			{
+				var context = _listener.BeginClient(OnContextReady, _listener);
+				if (0 == WaitHandle.WaitAny(new[] { _stop, context.AsyncWaitHandle }))
+					return;
+			}
+		}
+
+		private void OnContextReady(IAsyncResult ar)
+		{
+			try
+			{
+				object context;
+
+				lock (_lock)
+				{
+					// If not listening return immediately as this method is called on last time after Close()
+					if (!_listener.IsListening || _stoped)
+						return;
+
+					context = _listener.EndClient(ar);
+				}
+
+				_listener.ProcessRequest(context);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+			}
 		}
 	}
 }
